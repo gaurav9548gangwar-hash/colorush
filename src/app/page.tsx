dc3505b8b67992f7f07b463224e717c7d9b7c56d
Gaurav Gangwar <gaurav9548gangwar@gmail.com>
2025-11-05 08:51:11 +0000

    Ab bhi direct game open ho raha hai login page add kro mujhe ragistratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-
 'use client'
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useFirebase } from "@/firebase";
-
 
 export default function Home() {
   const router = useRouter();
-  const { user, isUserLoading } = useFirebase();
 
   useEffect(() => {
-    if (!isUserLoading) {
-        if (user) {
-            router.push('/dashboard');
-        } else {
-            router.push('/login');
-        }
-    }
-  }, [router, user, isUserLoading]);
+    router.push('/login');
+  }, [router]);
 
-  return <div className="flex items-center justify-center min-h-screen">Loading...</div>
+  return <div className="flex items-center justify-center min-h-screen">Redirecting to login...</div>
 }
